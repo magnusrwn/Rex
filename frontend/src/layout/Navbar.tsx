c1e9470e335c961a0ada11d4e4b350394b0be574
magnusrwn <119822862+magnusrwn@users.noreply.github.com>
2025-08-22 02:20:33 +0100

    Button component complete

--- frontend/src/layout/Navbar.tsx
+++ frontend/src/layout/Navbar.tsx
@@ -5,7 +5,9 @@ import Button from "../components/Button"
 const Navbar = () => {
     return (
         <>
-        <Button type="button" onClick={() => alert("you clicked my fattass")}>BUTTON HERE</Button>
+        <Button type="button" variant='primary'>primary</Button>
+        <Button type="button" variant='secondary'>secondary</Button>
+        <Button type="button" variant='negative' className="">negative</Button>
         <NavLink to='/' className='bg-amber-950'>Home</NavLink>
         <NavLink to='/login'>Login</NavLink>
         <NavLink to='/about'>About</NavLink>
